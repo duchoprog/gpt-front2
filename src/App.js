b21e5351d0c34fcb5eee1dde9850b602322d5e10
duchoprog <duchoprog@gmai.com>
2023-04-03 15:00:48 -0300

    start me up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Reader from "./pages/Reader";
+import Create from "./pages/Create";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reader" element={<Reader />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Reader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./reader.css";
+
+const Reader = () => {
+  return <div className="reader">Reader</div>;
+};
+
+export default Reader;
